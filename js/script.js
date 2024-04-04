72d56c5c3b713157dfb40158e1d80f75066dd810
prit patel <pritnpatel2510@gmail.com>
2024-04-04 11:16:34 +0530

    added first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function success(){
+    alert("Register Successfully!")
+}
+function contect(){
+    alert("thank you for conecting us!")
+}
+function test(){
+    alert("hello")
+    var ans=testform.answer.value
+    var ans2=testform.answer2.value
+    var marks=0
+    if (ans=true){
+         marks=marks+2
+    }
+    if(ans2=true){
+
+    }
+    alert(`${ans}`)
+}
